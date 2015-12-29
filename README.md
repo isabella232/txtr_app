df2cd137562a7933b36a951be26e8f599404aea8
Fabio Vilela <fbvilela@gmail.com>
2015-12-29 11:53:38 +1100

    first working version of the app

--- README.md
+++ README.md
@@ -12,3 +12,11 @@ Please submit bug reports to [Insert Link](). Pull requests are welcome.
 
 ### Screenshot(s):
 [put your screenshots down here.]
+
+
+
+app will update the json in settings
+hidden settings to store the key value pair
+initial value = a few shortcuts
+We will keep the shortcuts.json as reference
+vou ter que botar o shortcuts json no manifest como string.
--- app.js
+++ app.js
@@ -2,11 +2,29 @@
 
   return {
     events: {
-      'app.activated':'doSomething'
+      'comment.text.changed':'checkText'
     },
 
-    doSomething: function() {
+    checkText: _.debounce( function() {
+      shortcuts = JSON.parse(this.setting('shortcuts'));
+
+      shared = shortcuts['shared'];
+      user_shortcuts = shortcuts[this.currentUser().id()];
+      //for each of the shared shortcuts. replace
+      this.matchAndReplace(shared);
+
+      //for each user specific, replace.
+      this.matchAndReplace(user_shortcuts);
+
+    }, 200),
+
+    matchAndReplace: function(shortcuts){
+      for( element_key in shortcuts ){
+        regex = new RegExp("\\s" + element_key + "\\s", "g");
+        this.comment().text( this.comment().text().replace(regex, " " + shortcuts[element_key] + " ") );
+      }
     }
+
   };
 
 }());
